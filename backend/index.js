32cbeecc299d9518441b1dff9d88673c42956ae8
Jorge Jimenez <jorge970102@outlook.com>
2024-03-20 19:08:00 -0500

    Deploy

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,31 @@
-import express from 'express'
-import morgan from 'morgan'
-import router from './src/routes/index.js'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import { app, server } from './src/socket/socket.js'
-import mongoConnection from './src/database.js'
+import express from 'express';
+import path from 'path';
+import morgan from 'morgan';
+import router from './src/routes/index.js';
+import cookieParser from 'cookie-parser';
+import { app, server } from './src/socket/socket.js';
+import mongoConnection from './src/database.js';
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3000;
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+const __dirname = path.resolve();
 
-const corsOptions = {
-  origin: 'http://127.0.0.1:5173',
-  credentials: true 
-};
-app.use(cors(corsOptions));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
-app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 
 app.use('/api', router);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     mongoConnection();
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
