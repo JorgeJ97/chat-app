f65a5218ef139324831b4cd5245508047db97005
Jorge Jimenez <jorge970102@outlook.com>
2024-03-22 19:05:44 -0500

    fix bug getInitials

--- client/src/utils/getInitials.js
+++ client/src/utils/getInitials.js
@@ -1,7 +1,7 @@
 function getInitials(fullName) {
     let initials = ''
 
-    const nameArray = fullName.split(' ');
+    const nameArray = fullName.split(' ').filter(name => name !== '');
     if(nameArray.length >= 2){
         initials = nameArray[0][0] + nameArray[1][0];
     }else initials = nameArray[0][0] + nameArray[0][1];
